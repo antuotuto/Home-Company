c50fe480e82698c74c610d0cc0f3d494e6b55211
anan <675988829@qq.com>
2017-06-16 11:10:11 +0800

    Dfsd

--- an/src/pages/talk.vue
+++ an/src/pages/talk.vue
@@ -3,6 +3,10 @@
 
     
 
+    <audio src="http://dl.stream.qqmusic.qq.com/C400001CkBs10UxKxc.m4a?vkey=E656C1D9470995500BBE97B8875F3D9C5E0F15CB3A6328255A59C1386FFD2F3840F3672A3CDE94294A4AD1E02F0658272AA025C83F26DC93&guid=7543918651&uin=675988829&fromtag=66" controls="controls">
+    </audio>
+
+
   </div>
 </template>
 
